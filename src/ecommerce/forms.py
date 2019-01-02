ea524afa0b667da7c49bd47f4e21b8879d53576e
yoyoy74662000 <yoyoy74662000@github.com>
2019-01-02 02:32:42 -0600

    user login

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(
+        # to check out this in https://docs.djangoproject.com/en/2.1/ref/forms/widgets/#django.forms.TextInput
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your full name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Your email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                "placeholder": "Your message"
+            }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com")
+        return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,8 +1,73 @@
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm
+
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "I love you"
+    }
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "About page"
+    }
+    # Since home_page.html is a template, we use context to add things in it.
+    return render(request, "home_page.html", context)
+
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact page",
+        "content": "welcome to contact page",
+        "form": contact_form
+    }
+
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+#    if(request.method == 'POST'):
+#         print(request.POST)
+#         print(request.POST.get('fullname'))
+#         print(request.POST.get('email'))
+#         print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    # print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)  # it will show on terminal
+        username  = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        # print(request.user.is_authenticated())
+        if user is not None:
+            # print(request.user.is_authenticated())
+            login(request, user)
+            # Redirect to a success page.
+            # context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            # Return an 'invalid login' error message.
+            print("Error")
+
+    return render(request, "auth/login.html", context)
+
+
+def register_page(request):
+    return render(request, "auth/register.html", {})
+
 
 def home_page_old(request):
     html_ = """
